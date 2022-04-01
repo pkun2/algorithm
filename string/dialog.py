b0994832406b0be8cfdb26bf1120640e06b61087
pkun2 <pkun2@naver.com>
2022-04-01 10:48:40 +0900

    4월1일 공부

---
+++ string/dialog.py
@@ -0,0 +1,11 @@
+alphabet_list = ["ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "WXYZ"];
+time = 0;
+word = input();
+
+for a in alphabet_list:
+    for i in a:
+        for j in word:
+            if i == j:
+                time += alphabet_list.index(a) + 3;
+                
+print(time);

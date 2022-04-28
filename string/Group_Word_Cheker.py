a04304af78b929479f2b40aa8ad410d109c191de
pkun2 <pkun2@naver.com>
2022-04-28 10:36:24 +0900

    수정

--- string/Group_Word_Cheker.py
+++ string/Group_Word_Cheker.py
@@ -6,12 +6,10 @@ for i in range(cycle):
     word = input();
     for a in range(len(word)-1):
         if word[a] != word[a+1]:
-            new = word[a+1];
+            new = word[a+1:];
             if new.count(word[a]) > 0:
                 wrong +=1;
     if wrong == 0:
         wordContinue += 1;
 
 print(wordContinue);
-        
-   
